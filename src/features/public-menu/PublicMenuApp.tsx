1dbebe897c5a8cb830103badadbad2b825581cf8
Gian marco <gianmarcoing58@gmail.com>
2026-07-31 20:33:12 -0500

    feat: implement PublicMenuApp component; integrate PublicQueryProvider and PublicMenu for public menu display

---
+++ src/features/public-menu/PublicMenuApp.tsx
@@ -0,0 +1,10 @@
+import { PublicQueryProvider } from '../public-api/query/public-query-client';
+import { PublicMenu } from './components/PublicMenu';
+
+export function PublicMenuApp() {
+  return (
+    <PublicQueryProvider>
+      <PublicMenu />
+    </PublicQueryProvider>
+  );
+}
